b293e134a0777d9673295ac7525f079af3794388
ryserk <85703424+ryserk@users.noreply.github.com>
2021-12-29 20:07:25 -0300

    Update simplelogin.py

--- simplelogin.py
+++ simplelogin.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-sg.theme('Topanga')
+sg.theme('Topanga') #the colorscheme of pysimplegui, you can see more themes in the cookbook
 
 def principalapp():
     layout = [[sg.Text('Enter your user and password :'), sg.Text(size = (25, 1),key = '-OUTPUT-')],
@@ -15,12 +15,12 @@ def principalapp():
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         if event == 'Check':
-            if(values['-IN-'] == 'ryserk' and values['-NAME-'] == '123'):
+            if(values['-IN-'] == 'ryserk' and values['-NAME-'] == '123'): #here you can define your user and password inside the quotation marks
                 window['-OUTPUT-'].update("Connection accepted")
             else:
                 window['-OUTPUT-'].update("Connection not accepted")
         if event == 'Show Message':
-            sg.popup('Hello {}'.format(values['-NAME-']))
+            sg.popup('Hello {}'.format(values['-NAME-'])) #this line will show a popup with a message for the user
 
     window.close()
 
